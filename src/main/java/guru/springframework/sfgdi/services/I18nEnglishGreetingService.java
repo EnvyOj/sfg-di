2f5e408ac71e0f21d44327d5b9329f9eb1358088
Hunady Dominik <dominik.hunady@isdd.sk>
2021-03-14 15:19:11 +0100

    fixed issue, where english service returned constant string instead of value from repository

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -13,6 +13,6 @@ public class I18nEnglishGreetingService implements GreetingService {
 
     @Override
     public String sayGreeting() {
-        return "Hello World EN!!!";
+        return englishGreetingRepository.getGreeting();
     }
 }
